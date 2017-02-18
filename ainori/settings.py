a6634aa3805570daaac24e26450d0c26195765e9
Isaac <isaac@nicofun.club>
2017-02-18 18:11:59 +0900

    Add Twitter OAuth

--- ainori/settings.py
+++ ainori/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myauth.apps.MyauthConfig',
+    'social_django',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'social_django.middleware.SocialAuthExceptionMiddleware',
 ]
 
 ROOT_URLCONF = 'ainori.urls'
@@ -63,10 +65,20 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = [
+    'social_core.backends.twitter.TwitterOAuth',
+    'django.contrib.auth.backends.ModelBackend',
+]
+
+SOCIAL_AUTH_TWITTER_KEY = os.environ['TWITTER_KEY']
+SOCIAL_AUTH_TWITTER_SECRET = os.environ['TWITTER_SECRET']
 
 WSGI_APPLICATION = 'ainori.wsgi.application'
 
--- myauth/urls.py
+++ myauth/urls.py
@@ -1,9 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib.auth.views import login,logout
 
 urlpatterns = [
-    url(r'^login', login, {
+    url(r'^login/$', login, {
         'template_name': 'myauth/login.html',
     }, name='login'),
-    url(r'^logout', logout, name='logout'),
+    url(r'^twitter', include('social_django.urls', namespace = 'social')),
+    url(r'^logout/$', logout, name='logout'),
 ]
