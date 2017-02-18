b0f5ceee2d922998c0d287c39b7967b88dda7843
Isaac <isaac@nicofun.club>
2017-02-18 15:09:44 +0900

    Fix db setting

--- ainori/settings.py
+++ ainori/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ainori.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['POSTGRES_DB'],
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'HOST': 'db',
+        'PORT': 5432
     }
 }
 
